8b61e9e657b8278490ea94c1c6c195512e8a00f1
Andrew Martinez <martinez.s.andrew@gmail.com>
2025-10-03 22:46:32 -0500

    Added the programming language repository.

--- CodingTracker/Repository/ProgrammingLanguages/IProgrammingLanguageRepository.cs
+++ CodingTracker/Repository/ProgrammingLanguages/IProgrammingLanguageRepository.cs
@@ -1,3 +1,16 @@
-﻿namespace CodingTracker.Repository;
+﻿using CodingTracker.Models;
 
-public interface IProgrammingLanguageRepository { }
+namespace CodingTracker.Repository;
+
+public interface IProgrammingLanguageRepository
+{
+    public void InsertLanguage(ProgrammingLanguage language);
+
+    public List<ProgrammingLanguage> GetAllLanguages();
+
+    public ProgrammingLanguage GetLanguage(int id);
+
+    public void UpdateLanguage(ProgrammingLanguage language);
+
+    public void DeleteLanguage(int id);
+}
